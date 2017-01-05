c38d9575e4776561cd0caf1abca4d7086c7776c7
molin <531679708@qq.com>
2017-01-05 13:09:01 +0800

    日期

--- JCSwiftKit/Core/Foundation/NSDate/NSDate+JCDate.swift
+++ JCSwiftKit/Core/Foundation/NSDate/NSDate+JCDate.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+let kDATE_MINUTE_SEC = 60;      // 一分 = 60秒
+let kDATE_HOURS_SEC  = 3600;    // 一小时 = 60分 = 3600秒
+let kDATE_DAY_SEC    = 86400;   // 一天 = 24小时 = 86400秒
+let kDATE_WEEK_SEC   = 604800;  // 一周 = 7天 =  604800秒
+
+
 extension NSDate {
     
     var year: NSInteger {
@@ -88,12 +94,14 @@ extension NSDate {
         }
     }
     
+    /// 闰月
     var isLeapMonth: Bool {
         get {
             return DateComponents.init().isLeapMonth!;
         }
     }
     
+    /// 闰年
     var isLeapYear: Bool {
         get {
             let year = self.year;
@@ -101,6 +109,18 @@ extension NSDate {
         }
     }
     
+    /// 今天
+    var isToday: Bool {
+        get {
+            if (fabs(self.timeIntervalSinceNow) >= Double(kDATE_DAY_SEC)) {
+                return false;
+            }
+            return NSDate.init().day == self.day;
+        }
+    }
+}
+
+extension NSDate {
     
     class func dateWithString(stringDate: String) -> NSDate? {
         return NSDate.dateWithString(stringDate: stringDate, format: "yyyy-MM-dd HH:mm:ss");
@@ -111,5 +131,71 @@ extension NSDate {
         formatter.locale = NSLocale.current;
         formatter.dateFormat = format;
         return formatter.date(from: stringDate) as NSDate?;
+    }
+    
+    func string() -> String {
+        return self.stringWithFormat(format: "yyyy-MM-dd HH:mm:ss");
+    }
+    
+    func stringWithFormat(format: String) -> String {
+        let formatter = DateFormatter.init();
+        formatter.locale = NSLocale.current;
+        formatter.dateFormat = format;
+        return formatter.string(from: self as Date);
+    }
+    
+    /// 明天
+    class func dateTomorrow() -> NSDate {
+        return NSDate.dateWithDaysFromNow(days: 1);
+    }
+    
+    /// 后几天
+    class func dateWithDaysFromNow(days: NSInteger) -> NSDate {
+        return NSDate.init().dateByAdding(days: days);
+    }
+    
+    /// 昨天
+    class func dateYesterday() -> NSDate {
+        return NSDate.dateWithDaysBeforeNow(days: 1);
+    }
+    
+    /// 前几天
+    class func dateWithDaysBeforeNow(days: NSInteger) -> NSDate {
+        return NSDate.init().dateByAdding(days: -(days));
+    }
+    
+    /// 当前小时后hours个小时
+    class func dateWithHoursFromNow(hours: NSInteger) -> NSDate {
+        return NSDate.dateByAddingTimeInterval(ti: TimeInterval(kDATE_HOURS_SEC * hours));
+    }
+    
+    /// 当前小时前hours个小时
+    class func dateWithHoursBeforeNow(hours: NSInteger) -> NSDate {
+        return NSDate.dateByAddingTimeInterval(ti: TimeInterval(-kDATE_HOURS_SEC * hours));
+    }
+    
+    /// 当前分钟后minutes个分钟
+    class func dateWithMinutesFromNow(minutes: NSInteger) -> NSDate {
+        return NSDate.dateByAddingTimeInterval(ti: TimeInterval(kDATE_MINUTE_SEC * minutes));
+    }
+    
+    /// 当前分钟前minutes个分钟
+    class func dateWithMinutesBeforeNow(minutes: NSInteger) -> NSDate {
+        return NSDate.dateByAddingTimeInterval(ti: TimeInterval(-kDATE_MINUTE_SEC * minutes));
+    }
+    
+    /// 追加天数，生成新的NSDate
+    func dateByAdding(days: NSInteger) -> NSDate {
+        var dateComponents = DateComponents.init();
+        dateComponents.day = days;
+        let date = NSCalendar.current.date(byAdding: dateComponents, to: (self as Date?)!);
+        return (date as NSDate?)!;
+    }
+    
+    /// 追加秒数，生成新的NSDate
+    class func dateByAddingTimeInterval(ti: TimeInterval) -> NSDate {
+        let aTimeInterval = NSDate.init().timeIntervalSinceReferenceDate + ti;
+        let date = NSDate.init(timeIntervalSinceReferenceDate: aTimeInterval);
+        return date;
     }
 }
