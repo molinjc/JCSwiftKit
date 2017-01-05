c1f3c4f08927157e9706a8a8902e947076b5d740
molin <531679708@qq.com>
2017-01-05 16:32:09 +0800

    完善加载TTF字体

--- JCSwiftKit/Core/UIKit/UIFont/UIFont+JCScale.swift
+++ JCSwiftKit/Core/UIKit/UIFont/UIFont+JCScale.swift
@@ -10,7 +10,44 @@ import UIKit
 
 extension UIFont {
     
+    /// systemFontOfSize:
+    class func scaleFont(ofSize: CGFloat) -> UIFont {
+        let size = ofSize * UIScreen.main._scale_;
+        return UIFont.systemFont(ofSize: size);
+    }
+    
+    /// fontWithName:size:
+    class func scaleFont(name: String, ofSize: CGFloat) -> UIFont {
+        let size = ofSize * UIScreen.main._scale_;
+        return UIFont.init(name: name, size: size)!;
+    }
+    
+    /// 粗体
     class func fontNameWithHelveticaBold(size: CGFloat) -> UIFont {
-        return UIFont.init(name: "Helvetica-Bold", size: size)!;
+        return UIFont.scaleFont(name: "Helvetica-Bold", ofSize: size);
+    }
+    
+    /// 斜体
+    class func fontNameWithHelveticaOblique(size: CGFloat) -> UIFont {
+        return UIFont.scaleFont(name: "Helvetica-Oblique", ofSize: size);
+    }
+    
+    /// 加载本地TTF字体, 字体大小默认15.0f
+    class func fontWithTTF(atPath: String) -> UIFont {
+        return UIFont.fontWithTTF(atPath: atPath, size: 15.0);
+    }
+
+    /// 加载本地TTF字体
+    class func fontWithTTF(atPath: String, size: CGFloat) -> UIFont {
+        let foundFile = FileManager.default.fileExists(atPath: atPath);
+        if !foundFile {
+            return UIFont.systemFont(ofSize: size);
+        }
+        
+        let fontURL = CFURLCreateWithFileSystemPath(kCFAllocatorDefault, atPath as CFString!, CFURLPathStyle.cfurlposixPathStyle, false);
+        let dataProvider = CGDataProvider.init(url: fontURL!);
+        let graphicsFont = CGFont.init(dataProvider!);
+        let smallFont = CTFontCreateWithGraphicsFont(graphicsFont, size, nil, nil);
+        return smallFont as UIFont;
     }
 }
