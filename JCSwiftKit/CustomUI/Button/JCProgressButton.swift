a3793b91659e6060affd8a2dde821acb6c99b011
molin <531679708@qq.com>
2017-01-12 16:00:32 +0800

    有圆形进度条的按钮，待完善

---
+++ JCSwiftKit/CustomUI/Button/JCProgressButton.swift
@@ -0,0 +1,21 @@
+//
+//  JCProgressButton.swift
+//  JCSwiftKitDemo
+//
+//  Created by molin.JC on 2017/1/12.
+//  Copyright © 2017年 molin. All rights reserved.
+//
+
+import UIKit
+
+class JCProgressButton: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
