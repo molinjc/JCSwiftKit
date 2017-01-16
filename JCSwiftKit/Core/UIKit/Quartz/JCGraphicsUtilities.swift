ea11c416717c18f5323b47a9ea65a391d7f3c808
molin <531679708@qq.com>
2017-01-16 17:10:16 +0800

    画形状公共函数

--- JCSwiftKit/Core/UIKit/Quartz/JCGraphicsUtilities.swift
+++ JCSwiftKit/Core/UIKit/Quartz/JCGraphicsUtilities.swift
@@ -74,6 +74,11 @@ func JCDrawRectangle(rect: CGRect, color: UIColor) {
     _contextRef?.drawPath(using: CGPathDrawingMode.fill);
 }
 
+/// 画虚线
+func JCDrawDottedLine(p1: CGPoint, p2: CGPoint) {
+    JCDrawColorDottedLine(p1: p1, p2: p2, color: UIColor.gray);
+}
+
 /// 画有颜色的虚线
 func JCDrawColorDottedLine(p1: CGPoint, p2: CGPoint, color: UIColor) {
     let _contextRef = UIGraphicsGetCurrentContext();
@@ -86,4 +91,74 @@ func JCDrawColorDottedLine(p1: CGPoint, p2: CGPoint, color: UIColor) {
     _contextRef?.setLineDash(phase: 0, lengths: lengths);
     _contextRef?.move(to: p1);
     _contextRef?.addLine(to: p2);
+}
+
+/// 画直线
+func JCDrawLine(p1: CGPoint, p2: CGPoint, width: CGFloat, color: UIColor) {
+    JCDrawLineCap(p1: p1, p2: p2, width: width, color: color, cap: CGLineCap.round);
+}
+
+/// 画直线
+func JCDrawLineCap(p1: CGPoint, p2: CGPoint, width: CGFloat, color: UIColor, cap: CGLineCap) {
+    let _contextRef = UIGraphicsGetCurrentContext();
+    _contextRef?.setLineCap(cap);
+    _contextRef?.setLineWidth(width);
+    color.setStroke();
+    color.setFill();
+    _contextRef?.move(to: p1);
+    _contextRef?.addLine(to: p2);
+    _contextRef?.strokePath();
+}
+
+/// 类似刮刮乐的效果
+func JCImageScratch(layer: CALayer, size: CGSize, clearRect: CGRect) -> UIImage? {
+    UIGraphicsBeginImageContextWithOptions(size, false, 0);
+    let ref = UIGraphicsGetCurrentContext();
+    layer.render(in: ref!);
+    ref?.clear(clearRect);
+    let image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+/// 保留rect内的像素，之外不要，颜色为layer的背景颜色
+func JCImagePictureClip(layer: CALayer, rect: CGRect) -> UIImage? {
+    UIGraphicsBeginImageContextWithOptions(layer.frame.size, false, 0);
+    let path = UIBezierPath.init(rect: rect);
+    path.addClip();
+    let ref = UIGraphicsGetCurrentContext();
+    layer.render(in: ref!);
+    let image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return image;
+}
+
+/// 画一条波浪纹
+func JCWaveShapeLayer(shapeLayer: CAShapeLayer, amplitude: CGFloat, cycle: CGFloat, drift: CGFloat, size: CGSize) {
+    let wavePath = JCWavePathRef(amplitude: amplitude, cycle: cycle, drift: drift, size: size);
+    shapeLayer.path = wavePath;
+}
+
+/// 创建一条波浪纹路径
+///
+/// - Parameters:
+///   - amplitude: 振幅
+///   - cycle: 周期
+///   - drift: 位移
+///   - size: 波浪纹的高宽
+/// - Returns: 路径
+func JCWavePathRef(amplitude: CGFloat, cycle: CGFloat, drift: CGFloat, size: CGSize) -> CGMutablePath {
+    let wavePath = CGMutablePath.init();
+    var y = size.height;
+    wavePath.move(to: CGPoint.init(x: 0, y: y));
+    
+    for i in 0...Int(size.width) {
+        y = amplitude * sin(cycle * CGFloat(i) + drift) + size.height;
+        wavePath.addLine(to: CGPoint.init(x: CGFloat(i), y: y))
+    }
+    
+    wavePath.addLine(to: CGPoint.init(x: size.width, y: 0));
+    wavePath.addLine(to: CGPoint.zero);
+    wavePath.closeSubpath();
+    return wavePath;
 }
